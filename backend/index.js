bf0d4d23bd36ec2939a2362bfbb556a4d5a37015
aryanraj202 <134063935+aryanraj202@users.noreply.github.com>
2023-12-09 22:15:08 +0530

    added .env

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,9 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const speakeasy = require("speakeasy");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -11,9 +14,7 @@ app.use(cors());
 app.use(bodyParser.json());
 const sgMail = require("@sendgrid/mail");
 
-sgMail.setApiKey(
-  "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"
-);
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const connectToDB = async () => {
   await mongoose.connect(
